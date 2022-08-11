b9d3f9d0a08958045efbda628f31027da7ba00b3
Mateusz Nowak <novka.mateusz94@gmail.com>
2022-08-11 14:36:57 +0200

    Updating sample up

--- app/main.py
+++ app/main.py
@@ -1,29 +1,28 @@
 import logging
-import logging.handlers
+import sys
+import uuid
 
-
-import json_logging
 import sanic
-from sanic.log import logger
+
+
+async def generate_correlation_id(request: sanic.Request):
+    if 'correlation-id' not in request.headers:
+        request.headers['correlation-id'] = str(uuid.uuid4())
+
 
 app = sanic.Sanic(name="main-app")
 
-json_logging.init_sanic(enable_json=True)
-json_logging.init_request_instrument(app)
+logger = logging.getLogger("sanic-app")
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
-# init the logger as usual
-logger = logging.getLogger("main-app-logger")
-logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.basicConfig(filename="./logs/test.log"))
-
+app.register_middleware(generate_correlation_id, "request")
 
 @app.route("/")
-async def home(request):
-    # this will be faster
-    correlation_id = json_logging.get_correlation_id(request=request)
-
+async def home(request: sanic.Request):
+    logger.info(f"Incomming requests {request.headers['correlation-id']}")
     return sanic.response.text("hello world")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, access_log=True)
+    app.run(host="0.0.0.0", port=8000)
